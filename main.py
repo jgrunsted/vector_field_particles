dc771533f4a486c3a59189ad176bc3fb0a698277
Jaydon Grunsted <jgrunsted@gmail.com>
2022-09-10 10:42:03 -0400

    sss

--- main.py
+++ main.py
@@ -6,7 +6,7 @@ import particles
 
 app = Flask(__name__)
 
-particles = particles.Particles(np.array(["-x-y", "-y+x"]), 3500)
+particles = particles.Particles(np.array(["-x-y", "-y+x"]), 2000)
 # ["-2.0*(round(y) % 2.0) + 1.0", "-2.0*(round(x) % 2.0) + 1.0"]
 # ["-x-y", "-y+x"]
 
--- static/canvas.js
+++ static/canvas.js
@@ -9,12 +9,12 @@ const base_url = window.location.href;
 
 function animate() {
     $.getJSON(base_url + '/_update_pos', function(particle_data){
-        c.fillStyle = 'rgba(234,25,83,0.01)';
+        c.fillStyle = 'rgba(10,10,20,0.2)';
         c.fillRect(0, 0, 1900, 950);
         for (const [key, value] of Object.entries(particle_data)) {
             const pos_data = value.split(",");
             c.beginPath();
-            c.arc(pos_data[0].slice(2), pos_data[1].slice(0, -1), 0.1, 0, 2 * Math.PI, false);
+            c.arc(pos_data[0].slice(2), pos_data[1].slice(0, -1), 2, 0, 2 * Math.PI, false);
             c.fillStyle = `rgba(250, 250, 250, ${pos_data[2].slice(0, -1)})`;
             c.fill();
         }
@@ -23,7 +23,7 @@ function animate() {
     setTimeout(() => animate(), 18);
 }
 
-c.fillStyle = 'rgba(234,25,83,1)';
+c.fillStyle = 'rgba(10,10,20,1)';
 c.fillRect(0, 0, 1900, 950);
 
 animate();
